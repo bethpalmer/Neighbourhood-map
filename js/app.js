0c2759bbd77274ce350bfb9b9b30bcaca452c958
Beth Palmer <Beth@macbook-pro.home>
2017-05-23 22:20:43 +0100

    Fully functioning markers

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 // places[i].locations[i].name
 // places[i].locations[i].location
 
+// selectedCategory is poiList[0]
+// for (var locations in selectedCategory){
+//	console.log('${selectedCategory[locations]}')
+// }
 
 // places of interest
 var poi = [
@@ -107,134 +111,91 @@ var ViewModel = function(){
 
 	var self = this;
 
-	// Makes my model data observable and useable by the view via the ViewModel intermediary.
+	this.markers = [];
+
+	// Makes my model data observable and useable by the view via the ViewModel.
 	this.poiList = ko.observableArray([]); // observable array of objects
 
 	poi.forEach(function(object){
 		self.poiList.push(object);
 	});
-	// console.log(self.poiList());
 
-	this.selectedCategory = ko.observable(this.poiList()[null]);
+	this.selectedCategory = ko.observable();
 	// console.log("Category changed to "+self.selectedCategory.area);
 
 	this.places = ko.observableArray([]); // which needs to contain objects within the locations array containing name: and location:
 
-	this.selectCategory = function(clicked){
+	this.clickCategory = function(clicked){ // THIS SETS THE selectedCategory to one of the array indexes of poiList
 		self.selectedCategory(clicked);
-		// var elem = document.getElementById('list');
-		// elem.style.color = #ff0000; // defines selectedCategory as the object with area: and locations:
-		
 	};
-	// console.log(this.selectedCategory);
 
-	// this.getPlaces = function(selectedCategory, object){
-	// 	for (var i=0; i<selectedCategory.locations.length; i++){
-	// 		individualPlaceObj.locations.push(object);
-	// 	};
-	// };
-	// console.log(individualPlaceObj);
+	this.selectedCategory.subscribe(function(newValue){
+		self.showMarkers(newValue.area);
+	});
+
+	this.showMarkers = function(findArea) {
+
+		// clear markers
+		for (var i=0; i<self.markers.length; i++){
+			self.markers[i].setMap(null);
+		};
+		
+		self.markers = [];
+
+		// add markers
+		var bounds = new google.maps.LatLngBounds();
+
+		poi.forEach(function(area) {
+			if (findArea == null || area.area == findArea) {
+				area.locations.forEach(function(location) {
+
+					var marker = new google.maps.Marker({
+                        map: self.map,
+                        position: location.location,
+                        title: location.name,
+                        animation: google.maps.Animation.DROP
+                    });
+
+                    bounds.extend(location.location);
+
+                    self.markers.push(marker);
+
+				});
+			}
+		});
+
+		self.map.fitBounds(bounds);
+
+	};
+
+	this.init = function() {
+
+		self.map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 50.827022, lng: -0.158278},
+            zoom: 12
+        });
+       
+        var largeInfowindow = new google.maps.InfoWindow();
+        var bounds = new google.maps.LatLngBounds();
+
+        self.showMarkers(null);
+
+	};
+
 	// LOOK WITHIN THE OBJECT AND PACKAGE UP LOCATIONS[I].OBJECTS TO BE USED BY MAPS
 };
 
-ko.applyBindings(new ViewModel());
+var vm = new ViewModel();
+ko.applyBindings(vm);
 
 ///////////////////// NEXT STEPS ////////////////////////////
 /*
 GOAL: CREATE AND DISPLAY MARKERS FOR EACH CATEGORY WHEN THAT CATEGORY IS MARKED AS SELECTEDCATEGORY
+GOAL2: DISPLAY MARKERS FOR ALL CATEGORIES ON INITIAL LOAD
 */
 
 
 ////////////////////// MAP HANDLING //////////////////////////
 
-var map;
-var markers = [];
-// var musicMarkers = [];
-function initMap(){
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 50.827022, lng: -0.158278},
-        zoom: 12
-    });
-}
-   
-    // var largeInfowindow = new google.maps.InfoWindow();
-    // var bounds = new google.maps.LatLngBounds();
-    
-//     var places = [];
-
-//     poiList.forEach(function(obj){ 
-//         places.push(obj);
-//     });
-
-//     console.log(places);
-
-//     for (var i=0; i<places.length; i++){
-//          places[i].locations.forEach(function(object) {
-//              var position = location;
-//              var title = name;
-//              var marker = new google.maps.Marker({
-//                 map: map,
-//                 position: postion,
-//                 title: title,
-//                 animation: google.maps.Animation.DROP,
-//                 id: i
-//             });
-
-//             markers.push(marker);
-//             bounds.extend(marker.position);
-//             marker.addListener('click', function(){
-//                 populateInfoWindow(this, largeInfowindow);
-//             });
-//         });
-//     };               
-//     map.fitBounds(bounds);
-    // map.fitBounds(bounds); // won't set map bounds until we showHouses()
-    // document.getElementById('list_2').addEventListener('click', showLandmarks);
-    // document.getElementById('list_4').addEventListener('click', showMusic);
-// };
-// This function populates the infowindow when the marker is clicked. Only allows one info window to open, and populates based on markers position.
-// function populateInfoWindow(marker, infowindow){
-    // check to make sure infowindow is not already open
-    // if (infowindow.marker != marker) {
-    //     infowindow.marker = marker;
-    //     infowindow.setContent('<div>' + marker.title + '</div>');
-    //     infowindow.open(map, marker);
-        // make sure the marker property is cleared if the infowindow is closed
-    //     Infowindow.addListener('closeclick', function(){
-    //         infowindow.setMarker(null);
-    //     })
-    // }
-// }
-
-// function showAll(){
-//     var bounds = new google.maps.LatLngBounds();
-//     // Extend the boundaries of the map for each marker and display the marker
-//     for (var i=0; i<landmarkMarkers.length; i++){
-//         landmarkMarkers[i].setMap(map);
-//         bounds.extend(landmarkMarkers[i].position);
-//     }
-//     for (var i=0; i<musicMarkers.length; i++){
-//         musicMarkers[i].setMap(map);
-//         bounds.extend(musicMarkers[i].position);
-//     }
-//     map.fitBounds(bounds);
-// };
-// This function will loop through the markers array and display them all
-// function showLandmarks(){
-//     var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-//     for (var i=0; i<landmarkMarkers.length; i++){
-//         landmarkMarkers[i].setMap(map);
-//         bounds.extend(landmarkMarkers[i].position);
-//     }
-//     map.fitBounds(bounds);
-// }
-// function showMusic(){
-//     var bounds = new google.maps.LatLngBounds();
-//     for (var i=0; i<musicMarkers.length; i++){
-//         musicMarkers[i].setMap(map);
-//         bounds.extend(musicMarkers[i].position);
-//     }
-//     map.fitBounds(bounds);     
+//
 
